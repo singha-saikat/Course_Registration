463d7352e6ceb90bd5d10715d1fb3d851fa172f3
Saikat <sachinsingha7899@gmail.com>
2023-09-16 19:28:56 +0600

    Answer the Q2 in the README file

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,12 +2,6 @@ const Cart = ({ course, handleSelectBtn }) => {
   const { course_name, image_url, course_detail, course_price, credit_hours } =
     course;
 
-    // const handleSelectBTn = () =>{
-    //   handleSelectBtn(course);
-     
-    //   // handleCredit(id,credit_hours);
-
-    // };
   return (
     <div>
       <div className="card  bg-base-100 shadow-xl h-full p-2 ">
@@ -30,11 +24,8 @@ const Cart = ({ course, handleSelectBtn }) => {
                 <path
                   d="M12 6.042C10.3516 4.56336 8.2144 3.74694 6 3.75C4.948 3.75 3.938 3.93 3 4.262V18.512C3.96362 18.172 4.97816 17.9989 6 18C8.305 18 10.408 18.867 12 20.292M12 6.042C13.6483 4.56328 15.7856 3.74685 18 3.75C19.052 3.75 20.062 3.93 21 4.262V18.512C20.0364 18.172 19.0218 17.9989 18 18C15.7856 17.9969 13.6484 18.8134 12 20.292M12 6.042V20.292"
                   stroke="#1C1B1B"
-                 
                   strokeWidth="1.5"
-                 
                   strokeLinecap="round"
-                  
                   strokeLinejoin="round"
                 />
               </svg>
@@ -43,14 +34,12 @@ const Cart = ({ course, handleSelectBtn }) => {
           </div>
 
           <div>
-            
             <button
               onClick={() => handleSelectBtn(course)}
               className="btn  lg:btn-wide   bg-blue-500 mb-4"
             >
               Select
             </button>
-            
           </div>
         </div>
       </div>
